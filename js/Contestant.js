937d57b577dc1c1a069eee40462765577e8cd753
JASHWANTHYT <86235521+JASHWANTHYT@users.noreply.github.com>
2021-12-18 10:49:12 +0530

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,151 @@
+class Contestant {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  
+}
+
+/*
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120, 160);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Hello ${this.input.value()}
+      </br>wait for another player to join...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+      player.getDistance();
+    });
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handleMousePressed();
+  }
+}
+
+*/
+/*
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.rank = 0;
+    this.fuel = 185;
+    this.life = 185;
+    this.score = 0;
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score,
+      life: this.life
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score,
+      life: this.life //C41//SA
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+
+  getCarsAtEnd() {
+    database.ref("carsAtEnd").on("value", data => {
+      this.rank = data.val();
+    });
+  }
+
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
+  }
+}
+
+*/
